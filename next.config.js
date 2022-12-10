26677630bc3dc70bfaf0e17bbb19afd1b48728fc
王英杰 <wyjustc@mail.ustc.edu.cn>
2022-12-10 22:25:05 +0800

    [feat] sass模块中划线自动转驼峰

--- next.config.js
+++ next.config.js
@@ -1,9 +1,28 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     appDir: false
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  },
+  webpack: (config) => {
+    // camelCase style names from css modules
+    config.module.rules
+      .find(({ oneOf }) => !!oneOf).oneOf
+      .filter(({ use }) => JSON.stringify(use)?.includes('css-loader'))
+      .reduce((acc, { use }) => acc.concat(use), [])
+      .forEach(({ options }) => {
+        if (options.modules) {
+          options.modules.exportLocalsConvention = 'camelCase';
+        }
+      });
+
+    return config;
   }
 }
 
