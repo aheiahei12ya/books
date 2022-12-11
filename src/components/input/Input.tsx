412e19ee191753f3bffda67b931c1ac877826758
王英杰 <wyjustc@mail.ustc.edu.cn>
2022-12-11 19:49:00 +0800

    [feat] 输入框增加错误提示

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -109,8 +109,14 @@ export const Input = forwardRef<InputRef, InputProps>((props, ref) => {
           >
           { props.append }
         </span>
-      ) }
-    </div>
+        ) }
+      </div>
+      <div
+        className={ styles.inputWarning }
+      >
+        <span>aaa</span>
+      </div>
+    </>
   )
 })
 
