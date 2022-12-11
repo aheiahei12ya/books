0a739dc1554c6dfbf843ce0b74b49a708a3b0c4b
王英杰 <wyjustc@mail.ustc.edu.cn>
2022-12-11 18:37:17 +0800

    [feat] 提高输入框鲁棒性

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,5 +1,5 @@
 import styles from './Index.module.sass';
-import { InputRef, InputProps } from "./interface";
+import { InputRef, InputProps } from "./Input.types";
 import classNames from 'classnames';
 import { forwardRef, useState, useRef, useImperativeHandle, useId } from "react";
 
@@ -10,11 +10,15 @@ export const Input = forwardRef<InputRef, InputProps>((props, ref) => {
   const hasAppend = !!props.append
   const size = props.size || 'default'
   const [hasFocus, setHasFocus] = useState(false)
+  const [localValue, setLocalValue] = useState(props.value ? props.value : '')
   const inputRef = useRef<HTMLInputElement>(null)
   const inputId = useId()
 
-  const value = props.value !== undefined ? props.value : ''
-  const setValue = (val: string) => props?.onChange?.(val)
+  const [value, setValue] = (() => {
+    if (props.value !== undefined && props.onChange)
+      return [props.value, (val: string) => props.onChange!(val)]
+    return [localValue, setLocalValue]
+  })()
 
   useImperativeHandle(ref, () => ({
     clear: () => {
@@ -37,54 +41,72 @@ export const Input = forwardRef<InputRef, InputProps>((props, ref) => {
   })()
 
   return (
-    <div
-      className={
-        classNames(
-          styles.inputBox,
-          {
-            [styles.inputBoxSm]: size === 'small',
-            [styles.inputBoxLg]: size === 'large',
-            [styles.inputBoxBase]: size === 'default',
-            [styles.inputBoxFocus]: hasFocus
-          },
-        ) }
-      onClick={ () => inputRef.current?.focus() }
-    >
-      { hasPrepend && (
-        <span className={ styles.prefix }>
+    <>
+      <div
+        className={
+          classNames(
+            styles.inputBox,
+            {
+              [styles.inputBoxSm]: size === 'small',
+              [styles.inputBoxLg]: size === 'large',
+              [styles.inputBoxBase]: size === 'default',
+              [styles.inputBoxFocus]: hasFocus
+            },
+          ) }
+        onClick={ () => inputRef.current?.focus() }
+        onMouseDown={ e => {
+          e.preventDefault()
+        } }
+      >
+        { hasPrepend && (
+          <span
+            className={ styles.prefix }
+            onMouseDown={ e => {
+              e.preventDefault()
+            } }
+          >
           { props.prepend }
         </span>
-      ) }
-      <input
-        ref={ inputRef }
-        id={ inputId }
-        className={ styles.input }
-        placeholder={ props.placeholder }
-        value={ props.value && value }
-        onChange={ e => {
-          setValue(e.target.value)
-        } }
-        onFocus={ e => {
-          setHasFocus(true)
-        } }
-        onBlur={ e => {
-          setHasFocus(false)
-        } }
-        disabled={ props.disabled }
-        readOnly={ props.readOnly }
-      />
-      { shouldShowClear && (
-        <span className={ styles.clearable }
-              onClick={ () => setValue('') }
-              onMouseDown={ e => {
-                e.preventDefault()
-              } }
-        >
+        ) }
+        <input
+          ref={ inputRef }
+          id={ inputId }
+          className={ styles.input }
+          placeholder={ props.placeholder }
+          value={ value }
+          onChange={ e => {
+            setValue(e.target.value)
+          } }
+          onFocus={ e => {
+            setHasFocus(true)
+          } }
+          onBlur={ e => {
+            setHasFocus(false)
+          } }
+          disabled={ props.disabled }
+          readOnly={ props.readOnly }
+        />
+        { shouldShowClear && (
+          <span className={
+            classNames(
+              styles.clearable,
+              { [styles.clearableAppend]: hasAppend }
+            ) }
+                onClick={ () => setValue('') }
+                onMouseDown={ e => {
+                  e.preventDefault()
+                } }
+          >
           <i className="fa-solid fa-close"></i>
         </span>
-      ) }
-      { hasAppend && (
-        <span className={ styles.append }>
+        ) }
+        { hasAppend && (
+          <span
+            className={ styles.append }
+            onMouseDown={ e => {
+              e.preventDefault()
+            } }
+          >
           { props.append }
         </span>
       ) }
--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,5 +1,5 @@
 import { Input } from './Input';
 
-export type { InputProps, InputRef } from './interface';
+export type { InputProps, InputRef } from './Input.types';
 
 export default Input;
