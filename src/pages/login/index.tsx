98c08f99c2763d1e3c27df990c75809c9df510b0
王英杰 <wyjustc@mail.ustc.edu.cn>
2022-12-31 22:45:44 +0800

    [feat] 登录失败展示错误信息

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,31 +1,74 @@
 import classNames from 'classnames'
-import React, { useRef, useState } from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect, useRef, useState } from 'react'
 import { FormattedMessage, useIntl } from 'react-intl'
 
 import Button from '@/components/button'
 import Checkbox from '@/components/checkbox'
 import Input, { InputRef } from '@/components/input'
+import { accountError, passwordError } from '@/constants/resultCode'
+import useRequest from '@/hooks/useRequest'
+import { useAuth } from '@/lib/auth'
 import services from '@/services'
 
 import styles from './index.module.sass'
 
 const Login: React.FC = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [remember, setRemember] = useState(true)
-  const userIcon = <i className="fa-light fa-user"></i>
-  const passwordIcon = <i className="fa-light fa-lock"></i>
+  Login.displayName = 'Login'
   const i18n = useIntl()
+  const router = useRouter()
+  const auth = useAuth()
+
   const usernameRef = useRef<InputRef>(null)
   const passwordRef = useRef<InputRef>(null)
 
-  const handleSubmit = async () => {
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [remember, setRemember] = useState<boolean>(true)
+
+  const [usernameIncorrect, setUsernameIncorrect] = useState<boolean>(false)
+  const [passwordIncorrect, setPasswordIncorrect] = useState<boolean>(false)
+
+  const {
+    loading,
+    run: doLogin,
+    data,
+    error
+  } = useRequest(
+    () =>
+      services.login.login({
+        username: username,
+        password: password
+      }),
+    {
+      manual: true,
+      onSuccess: (data) => {
+        if (!data.success) {
+          data.resultCode === accountError && setUsernameIncorrect(true)
+          data.resultCode === passwordError && setPasswordIncorrect(true)
+        } else {
+          auth.setLogin(true)
+          auth.setUserInfo(data.data)
+        }
+      }
+    }
+  )
+
+  const handleSubmit = () => {
     const usernameError = usernameRef.current?.touch()
     const passwordError = passwordRef.current?.touch()
-    if (!usernameError && !passwordError) {
-      console.log(await services.login.submit())
+    if (!usernameError && !passwordError && !loading) {
+      doLogin()
     }
   }
+
+  useEffect(() => {
+    router.prefetch('/home')
+    if (data?.success) {
+      sessionStorage.setItem('userInfo', JSON.stringify(data.data))
+      router.replace('/home')
+    }
+  }, [data, router])
 
   return (
     <div className={ styles.login }>
@@ -41,9 +84,12 @@ const Login: React.FC = () => {
           <Input
             ref={ usernameRef }
             value={ username }
-            onChange={ (val: string) => setUsername(val) }
+            onChange={ (val: string) => {
+              setUsername(val)
+              setUsernameIncorrect(false)
+            } }
             clearable={ true }
-            prepend={ userIcon }
+            prepend={ <i className="fa-light fa-user"></i> }
             placeholder={ i18n.formatMessage({
               id: 'pages.login.username.placeholder'
             }) }
@@ -53,14 +99,21 @@ const Login: React.FC = () => {
                 message: <FormattedMessage id="pages.login.username.required"/>
               }
             ] }
+            error={ usernameIncorrect }
+            errorMessage={
+              <FormattedMessage id={ 'pages.login.username.incorrect' }/>
+            }
           ></Input>
           <Input
             ref={ passwordRef }
             value={ password }
             type={ 'password' }
-            onChange={ (val: string) => setPassword(val) }
+            onChange={ (val: string) => {
+              setPassword(val)
+              setPasswordIncorrect(false)
+            } }
             clearable={ true }
-            prepend={ passwordIcon }
+            prepend={ <i className="fa-light fa-lock"></i> }
             placeholder={ i18n.formatMessage({
               id: 'pages.login.password.placeholder'
             }) }
@@ -72,6 +125,10 @@ const Login: React.FC = () => {
                 )
               }
             ] }
+            error={ passwordIncorrect }
+            errorMessage={
+              <FormattedMessage id={ 'pages.login.password.incorrect' }/>
+            }
           ></Input>
           <div className={ styles.loginPanelExtra }>
             <Checkbox
@@ -90,7 +147,12 @@ const Login: React.FC = () => {
               ></FormattedMessage>
             </Button>
           </div>
-          <Button block color={ 'primary' } onClick={ handleSubmit }>
+          <Button
+            block
+            color={ 'primary' }
+            onClick={ handleSubmit }
+            loading={ loading }
+          >
             <FormattedMessage
               id={ 'pages.login.button.login' }
             ></FormattedMessage>
