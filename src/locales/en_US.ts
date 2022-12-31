f2000942d99bd0f320a0b749b871769abe7fc11e
王英杰 <wyjustc@mail.ustc.edu.cn>
2022-12-31 22:44:27 +0800

    [feat] 多语言

--- src/locales/en_US.ts
+++ src/locales/en_US.ts
@@ -4,7 +4,9 @@ export const en_US = {
   'pages.login.password.placeholder': 'Please enter your password',
   'pages.login.username.required': 'Please enter your username!',
   'pages.login.password.required': 'Please enter your password!',
+  'pages.login.username.incorrect': 'Please enter correct username!',
+  'pages.login.password.incorrect': 'Please enter correct password!',
   'pages.login.button.login': 'Log In',
   'pages.login.checkbox.remember': 'Remember Password',
-  'pages.login.button.forget': 'Forget Password?',
+  'pages.login.button.forget': 'Forget Password?'
 }
--- src/locales/zh_CN.ts
+++ src/locales/zh_CN.ts
@@ -4,7 +4,9 @@ export const zh_CN = {
   'pages.login.password.placeholder': '请输入密码',
   'pages.login.username.required': '请输入用户名!',
   'pages.login.password.required': '请输入密码!',
+  'pages.login.username.incorrect': '请输入正确的用户名!',
+  'pages.login.password.incorrect': '请输入正确的密码!',
   'pages.login.button.login': '登录',
   'pages.login.checkbox.remember': '记住密码',
-  'pages.login.button.forget': '忘记密码？',
+  'pages.login.button.forget': '忘记密码？'
 }
