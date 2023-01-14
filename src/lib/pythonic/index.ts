b7ae0c606547c15fab1cacde1aab909f21334333
王英杰 <wyjustc@mail.ustc.edu.cn>
2023-01-14 15:10:56 +0800

    [feat] pythonic方法

---
+++ src/lib/pythonic/index.ts
@@ -0,0 +1,5 @@
+import range from './range'
+import unzip from './unzip'
+import zip from './zip'
+
+export { range, unzip, zip }
---
+++ src/lib/pythonic/range.ts
@@ -0,0 +1,18 @@
+export default function range(start: number, stop?: number, step?: number) {
+  if (stop == null) {
+    stop = start || 0
+    start = 0
+  }
+  if (!step) {
+    step = stop < start ? -1 : 1
+  }
+
+  const length = Math.max(Math.ceil((stop - start) / step), 0)
+  const range = Array(length)
+
+  for (let idx = 0; idx < length; idx++, start += step) {
+    range[idx] = start
+  }
+
+  return range
+}
