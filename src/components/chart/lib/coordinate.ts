bf6348a4890e41e90210b82c4109a26debfc6c97
王英杰 <wyjustc@mail.ustc.edu.cn>
2023-01-18 21:36:23 +0800

    [feat] 坐标刻度生成方式优化

--- src/components/chart/lib/coordinate.ts
+++ src/components/chart/lib/coordinate.ts
@@ -1,4 +1,4 @@
-import { range } from '@/lib/pythonic'
+import { getValue, zip } from '@/lib/pythonic'
 
 export function setCoordinate(
   ctx: CanvasRenderingContext2D,
@@ -6,49 +6,53 @@ export function setCoordinate(
   canvasWidth: number,
   xs: number[],
   ys: number[],
-  hideAxes: boolean | undefined,
-  paddingX: number,
-  paddingY: number,
-  lineColor: string,
-  type?: string
+  paddingX?: number,
+  paddingTop?: number,
+  paddingBottom?: number,
+  hideAxes?: boolean,
+  type?: string | undefined,
+  lineColor?: string | undefined
 ) {
   const dpr = window.devicePixelRatio
-  const _paddingX = paddingX * dpr
-  const _paddingY = paddingY * dpr
-  const _splitNumber = type === 'bar' ? xs.length : xs.length
-  const gap = (canvasWidth - paddingX * 2 * dpr) / _splitNumber
-  const halfGap = gap / 2
-  const fontSize = 14
+  const _paddingTop = getValue(paddingTop, 0) * dpr
+  const _paddingBottom = getValue(paddingBottom, 0) * dpr
 
-  const xTicks = range(_splitNumber).map((i) => {
-    let x = i ? i * gap + _paddingX : _paddingX
-    if (type === 'bar') {
-      x += halfGap
+  let _paddingX = getValue(paddingX, 0) * dpr
+  const _splitNumber = type === 'bar' ? xs.length : xs.length - 1
+  const gap = (canvasWidth - _paddingX * 2) / _splitNumber
+  const halfGap = gap / 2
+  if (type === 'bar') {
+    _paddingX += halfGap
+  }
+
+  const fontSize = 14
+  const yRatio = (canvasHeight - _paddingBottom - _paddingTop) / Math.max(...ys)
+
+  const xTicks: number[] = []
+  const yTicks: number[] = []
+
+  zip(xs, ys).forEach(([x, y], i) => {
+    const xTick = i ? i * gap + _paddingX : _paddingX
+    const yTick = canvasHeight - y * yRatio - _paddingBottom
+    xTicks.push(xTick)
+    yTicks.push(yTick)
+
+    if (!hideAxes) {
+      ctx.beginPath()
+      ctx.moveTo(xTick, _paddingTop)
+      ctx.lineTo(xTick, canvasHeight - _paddingBottom)
+      ctx.strokeStyle = lineColor || '#aeaeae'
+      ctx.stroke()
+      ctx.closePath()
     }
 
-    // if (!hideAxes) {
-    ctx.beginPath()
-    ctx.moveTo(x, _paddingY / 2)
-    ctx.lineTo(x, canvasHeight - _paddingY / 2)
-    ctx.strokeStyle = lineColor
-    ctx.stroke()
-    ctx.closePath()
-    // }
-
-    if (!(i % 3)) {
+    if (!(i % 3) || i === xs.length - 1) {
       ctx.font = `${fontSize}px PingFangSC-Regular`
       ctx.textBaseline = 'bottom'
       ctx.fillStyle = '#fff'
-      ctx.fillText(i + 1, x - fontSize / 2, canvasHeight)
+      ctx.fillText(x.toString(), xTick - fontSize / 2, canvasHeight)
     }
-    return x
-  })
-
-  const yRatio = (canvasHeight - _paddingY) / Math.max(...ys)
-  const yTicks = ys.map((y) => {
-    return canvasHeight - y * yRatio - _paddingY / 2
   })
 
   return [xTicks, yTicks]
 }
-
