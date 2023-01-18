0d3157e511a83b5fbd9cdf5bd04a700de002ec72
王英杰 <wyjustc@mail.ustc.edu.cn>
2023-01-19 00:48:24 +0800

    [fix] 柱状图bug修复

--- src/components/chart/lib/barRect.ts
+++ src/components/chart/lib/barRect.ts
@@ -7,30 +7,22 @@ export const drawRect = (
   domHeight: number,
   domWidth: number,
   barWidth: number,
-  paddingTop: number,
   paddingBottom: number
 ) => {
   const dpr = window.devicePixelRatio
   const radius = Math.abs(Math.ceil(barWidth / 3.5))
-  const _paddingTop = paddingTop * dpr
   const _paddingBottom = paddingBottom * dpr
 
   zip(xs, ys).forEach(([x, y]) => {
     ctx.roundRect
       ? ctx.roundRect(
           x - barWidth / 2,
-          y - _paddingTop,
+          y,
           barWidth,
-          domHeight - y - _paddingTop - _paddingBottom,
+          domHeight - y - _paddingBottom,
           [radius, radius, 0, 0]
         )
-      : ctx.rect(
-          x - barWidth / 2,
-          y - _paddingTop,
-          barWidth,
-          domHeight - y - _paddingTop - _paddingBottom
-        )
-
+      : ctx.rect(x - barWidth / 2, y, barWidth, domHeight - y - _paddingBottom)
     const lineGradient = ctx.createLinearGradient(0, 0, 0, domHeight)
     lineGradient.addColorStop(0.3, '#003399')
     lineGradient.addColorStop(1, '#4066b3')
