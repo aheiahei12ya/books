addbf7da1b2712220016f1526cd137e8c81ab35b
王英杰 <wyjustc@mail.ustc.edu.cn>
2023-01-18 19:01:59 +0800

    [feat] chart增加样式变量

--- src/components/chart/lib/coordinate.ts
+++ src/components/chart/lib/coordinate.ts
@@ -4,23 +4,51 @@ export function setCoordinate(
   ctx: CanvasRenderingContext2D,
   canvasHeight: number,
   canvasWidth: number,
-  splitNumber: number,
+  xs: number[],
+  ys: number[],
   hideAxes: boolean | undefined,
   paddingX: number,
   paddingY: number,
-  lineColor: string
+  lineColor: string,
+  type?: string
 ) {
-  const gap = (canvasWidth - paddingX * 2) / (splitNumber - 1)
-  return range(splitNumber).map((i) => {
-    const x = i ? i * gap + paddingX : paddingX
-    if (!hideAxes) {
-      ctx.beginPath()
-      ctx.moveTo(x, paddingY)
-      ctx.lineTo(x, canvasHeight - paddingY)
-      ctx.strokeStyle = lineColor
-      ctx.stroke()
-      ctx.closePath()
+  const dpr = window.devicePixelRatio
+  const _paddingX = paddingX * dpr
+  const _paddingY = paddingY * dpr
+  const _splitNumber = type === 'bar' ? xs.length : xs.length
+  const gap = (canvasWidth - paddingX * 2 * dpr) / _splitNumber
+  const halfGap = gap / 2
+  const fontSize = 14
+
+  const xTicks = range(_splitNumber).map((i) => {
+    let x = i ? i * gap + _paddingX : _paddingX
+    if (type === 'bar') {
+      x += halfGap
+    }
+
+    // if (!hideAxes) {
+    ctx.beginPath()
+    ctx.moveTo(x, _paddingY / 2)
+    ctx.lineTo(x, canvasHeight - _paddingY / 2)
+    ctx.strokeStyle = lineColor
+    ctx.stroke()
+    ctx.closePath()
+    // }
+
+    if (!(i % 3)) {
+      ctx.font = `${fontSize}px PingFangSC-Regular`
+      ctx.textBaseline = 'bottom'
+      ctx.fillStyle = '#fff'
+      ctx.fillText(i + 1, x - fontSize / 2, canvasHeight)
     }
     return x
   })
+
+  const yRatio = (canvasHeight - _paddingY) / Math.max(...ys)
+  const yTicks = ys.map((y) => {
+    return canvasHeight - y * yRatio - _paddingY / 2
+  })
+
+  return [xTicks, yTicks]
 }
+
