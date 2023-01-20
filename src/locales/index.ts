80413ccb84e5d428ab7b3e0ef79215ca0d924eac
王英杰 <wyjustc@mail.ustc.edu.cn>
2023-01-20 19:09:52 +0800

    [feat] 全局样式优化

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,7 +2,7 @@ import { en_US } from './en_US'
 import { zh_CN } from './zh_CN'
 
 function loadLocale(lang: string) {
-  let message = null
+  let message
   switch (lang) {
     case 'en-US':
       message = en_US
