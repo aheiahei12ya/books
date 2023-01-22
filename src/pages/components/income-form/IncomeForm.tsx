6e7b75b1c2b7ba502232710030f66063833687cc
王英杰 <wyjustc@mail.ustc.edu.cn>
2023-01-23 00:01:19 +0800

    [feat] 收入表单

---
+++ src/pages/components/income-form/IncomeForm.tsx
@@ -0,0 +1,15 @@
+import { forwardRef } from 'react'
+
+import styles from './IncomeForm.module.sass'
+import { IncomeFormProps } from './IncomeForm.types'
+
+const IncomeForm = forwardRef<unknown, IncomeFormProps>(
+  (props, ref) => {
+    IncomeForm.displayName = 'IncomeForm'
+    return <div className={styles.incomeForm}>
+
+    </div>
+  }
+)
+
+export { IncomeForm }
---
+++ src/pages/components/income-form/index.tsx
@@ -0,0 +1,5 @@
+import { IncomeForm } from './IncomeForm'
+
+export type { IncomeFormProps } from './IncomeForm.types'
+
+export default IncomeForm
