a35e336cd4a63d015cbdd86b0e8ab3b39d3d2a33
王英杰 <wyjustc@mail.ustc.edu.cn>
2023-01-26 16:42:20 +0800

    [feat] 受控组件hook

---
+++ src/hooks/useControlled/index.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+const voidFunction = () => {
+}
+
+const useControlled = (value: any, onChange: Function | undefined) => {
+  const controlled = value !== undefined
+  const [innerValue, setInnerValue] = useState()
+
+  if (controlled) return [value, onChange || voidFunction]
+  return [innerValue, (newValue: any, ...args: any) => {
+    setInnerValue(newValue)
+    onChange?.(newValue, ...args)
+  }]
+}
+
+export default useControlled
