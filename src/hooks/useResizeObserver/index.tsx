823944d52b8336618607ceed89a21ca478fd7a82
王英杰 <wyjustc@mail.ustc.edu.cn>
2023-01-28 22:40:20 +0800

    [fix] 修复useObserver失效

--- src/hooks/useResizeObserver/index.tsx
+++ src/hooks/useResizeObserver/index.tsx
@@ -1,13 +1,10 @@
 import { RefObject, useLayoutEffect } from 'react'
 
-const useResizeObserver = (
-  callback: ResizeObserverCallback,
-  ref: RefObject<HTMLElement>
-) => {
+const useResizeObserver = (callback: Function, ref: RefObject<HTMLElement>) => {
   useLayoutEffect(() => {
     let resizeObserver: ResizeObserver
     if (typeof window.ResizeObserver === 'undefined' || !ref.current) return
-    resizeObserver = new window.ResizeObserver((entries) => callback)
+    resizeObserver = new window.ResizeObserver(() => callback())
     resizeObserver.observe(ref.current)
     return () => {
       resizeObserver.disconnect?.()
