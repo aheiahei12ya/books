9ef382e82a60dc90d66ae13b61914d7bcb88f179
王英杰 <wyjustc@mail.ustc.edu.cn>
2023-01-29 13:46:36 +0800

    [fix] ResizeObserver loop completed with undelivered notifications.

--- src/hooks/useResizeObserver/index.tsx
+++ src/hooks/useResizeObserver/index.tsx
@@ -2,11 +2,21 @@ import { RefObject, useLayoutEffect } from 'react'
 
 const useResizeObserver = (callback: Function, ref: RefObject<HTMLElement>) => {
   useLayoutEffect(() => {
+    const element = ref.current!
     let resizeObserver: ResizeObserver
     if (typeof window.ResizeObserver === 'undefined' || !ref.current) return
-    resizeObserver = new window.ResizeObserver(() => callback())
-    resizeObserver.observe(ref.current)
+    resizeObserver = new window.ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        requestAnimationFrame(() => {
+          if (element.offsetParent !== null) {
+            callback()
+          }
+        })
+      })
+    })
+    resizeObserver.observe(element)
     return () => {
+      resizeObserver.unobserve(element)
       resizeObserver.disconnect?.()
     }
   }, [ref, callback])
