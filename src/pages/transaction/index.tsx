2e92612b139dee3d7e8c6918a6934de8c6a55724
王英杰 <wyjustc@mail.ustc.edu.cn>
2023-03-11 19:23:49 +0800

    [feat] 账页面本更新

--- src/pages/transaction/index.tsx
+++ src/pages/transaction/index.tsx
@@ -1,15 +1,40 @@
 import classNames from 'classnames'
 import Head from 'next/head'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useIntl } from 'react-intl'
 
+import RecordForm from '@/components/biz/record-form'
 import RecordStatistic from '@/components/biz/record-statistic'
+import Button from '@/components/button'
 import Card from '@/components/card'
+import Dropdown from '@/components/dropdown'
+import Tag from '@/components/tag'
 import { range } from '@/lib/pythonic'
 import styles from '@/pages/transaction/index.module.scss'
 
 const Transaction = () => {
   const i18n = useIntl()
+  const tools = useMemo(() => {
+    return (
+      <>
+        <span className={styles.date}>2023.3.1 ~ 2023.3.31</span>
+        <div className={styles.toolButtons}>
+          <Tag hideBorder color={'success'} size={'small'}>
+            <span>本月支出: </span>
+            <span className={styles.outcome}>1000.00</span>
+          </Tag>
+          <Tag hideBorder color={'danger'} size={'small'}>
+            <span>本月收入: </span>
+            <span className={styles.income}>1000.00</span>
+          </Tag>
+          <Tag hideBorder color={'primary'} size={'small'}>
+            <span>本月结余: </span>
+            <span className={styles.balance}>1000.00</span>
+          </Tag>
+        </div>
+      </>
+    )
+  }, [])
   return (
     <>
       <Head>
@@ -17,30 +42,46 @@ const Transaction = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <main className={classNames(styles.page)}>
-        <Card title={'张静静的口袋'} subtitle={'今天是你记账的第1000天'}></Card>
-        <div className={styles.pageContainer}>
-          <div className={styles.columnLeft}>
-            <Card elevation={2} fill></Card>
+        <div className={styles.layerLeft}>
+          <div className={styles.greet}>
+            <Card title={'张静静的口袋'} subtitle={'今天是你记账的第1000天'}></Card>
           </div>
-          <div className={styles.columnMiddle}>
-            {/*<Card elevation={ 2 } fill>*/}
-            <Card bodyClass={styles.middleCard} elevation={2}>
-              {range(100).map((i) => (
-                <div key={i}>
-                  <p>{i}</p>
-                </div>
-              ))}
-            </Card>
-            {/*</Card>*/}
+          <div className={styles.columnContainer}>
+            <div className={styles.columnLeft}>
+              <Card title={'收支统计'} elevation={2}>
+                <RecordStatistic></RecordStatistic>
+              </Card>
+              <Card title={'账户统计'} elevation={2}>
+                <RecordStatistic></RecordStatistic>
+              </Card>
+            </div>
+            <div className={styles.columnRight}>
+              <Card
+                className={styles.filterContainer}
+                title={'收支明细'}
+                elevation={2}
+                toolClass={styles.toolBar}
+                tools={tools}
+                bodyClass={styles.filterSheet}
+              >
+                <Dropdown placeholder={'收支类型'} hideMessage></Dropdown>
+                <Dropdown placeholder={'账户名称'} hideMessage></Dropdown>
+                <Dropdown placeholder={'支出平台'} hideMessage></Dropdown>
+                <Dropdown placeholder={'一级分类'} hideMessage></Dropdown>
+                <Dropdown placeholder={'二级分类'} hideMessage></Dropdown>
+                <Button>重置</Button>
+              </Card>
+
+              <Card className={styles.recordContainer} bodyClass={styles.recordList} elevation={2}>
+                {range(100).map((i) => (
+                  <div key={i}>{i}</div>
+                ))}
+              </Card>
+            </div>
           </div>
-          <div className={styles.columnRight}>
-            <Card title={'收支统计'} elevation={1}>
-              <RecordStatistic></RecordStatistic>
-            </Card>
-            <Card title={'账户统计'} elevation={1}>
-              <RecordStatistic></RecordStatistic>
-            </Card>
-          </div>
+        </div>
+        <div className={styles.layerRight}>
+          <RecordForm orientation={'portrait'}></RecordForm>
         </div>
       </main>
     </>
