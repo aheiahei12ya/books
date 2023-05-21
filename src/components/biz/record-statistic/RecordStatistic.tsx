1893e7f98d8686c7cd127bcd6c2ac367e2c390c2
王英杰 <wyjustc@mail.ustc.edu.cn>
2023-05-21 12:14:39 +0800

    [feat] 分类统计接口

--- src/components/biz/record-statistic/RecordStatistic.tsx
+++ src/components/biz/record-statistic/RecordStatistic.tsx
@@ -1,70 +1,87 @@
-import { forwardRef } from 'react'
+import { forwardRef, useCallback, useMemo, useState } from 'react'
 
 import Progress from '@/components/progress'
 import Tag from '@/components/tag'
+import useRequest from '@/hooks/useRequest'
+import services from '@/services'
 
 import styles from './RecordStatistic.module.scss'
-import { RecordStatisticProps, RecordStatisticRef } from './RecordStatistic.types'
-
-const headList = [
-  { itemName: '本月收入', amount: 3333.33, percentage: 84.3 },
-  { itemName: '本月支出', amount: 88888.88, percentage: 84.3 }
-]
-
-const itemList = [
-  { itemName: '日常开销', amount: 3213.23, percentage: 84.3 },
-  { itemName: '住房开销', amount: 3133.23, percentage: 84.3 },
-  { itemName: '意外开销', amount: 2133.23, percentage: 84.3 },
-  { itemName: '设备引进', amount: 133.23, percentage: 84.3 },
-  { itemName: '人物培养', amount: 133.23, percentage: 84.3 },
-  { itemName: '生活开销', amount: 133.23, percentage: 84.3 },
-  { itemName: '虚拟商品', amount: 33.23, percentage: 84.3 },
-  { itemName: '败家消费', amount: 133.23, percentage: 84.3 },
-  { itemName: '出行开销', amount: 333.23, percentage: 84.3 }
-]
+import { classificationType, RecordStatisticProps, RecordStatisticRef } from './RecordStatistic.types'
 
 const RecordStatistic = forwardRef<RecordStatisticRef, RecordStatisticProps>((props, ref) => {
-  const width = `${Math.max(headList[0].amount, headList[1].amount).toString().length * 7.5}px`
-  const makeRow = (
-    itemName: string,
-    amount: number,
-    percentage: number,
-    bgColor: 'success' | 'warning' | 'danger' = 'warning',
-    hideText: boolean = false
-  ) => {
-    const text = hideText ? undefined : `${percentage}%`
-    return (
-      <div className={styles.recordStatisticRow} key={itemName}>
-        <div className={styles.recordStatisticRowTag}>
-          <Tag shape={'round'} size={'small'} select>
-            {itemName}
-          </Tag>
-        </div>
-        <div className={styles.recordStatisticRowProgress}>
-          <Progress
-            size={'small'}
-            text={text}
-            percentage={percentage}
-            backgroundColor={bgColor}
-            textColor={'#000'}
-          ></Progress>
-        </div>
-        <div className={styles.recordStatisticRowTag}>
-          <Tag size={'small'} width={width}>
-            {amount}
-          </Tag>
-        </div>
-      </div>
-    )
-  }
-  return (
-    <div className={styles.recordStatisticContainer}>
-      {headList.map((item, index) =>
-        makeRow(item.itemName, item.amount, item.percentage, index ? 'success' : 'danger', true)
-      )}
-      {itemList.sort((a, b) => b.amount - a.amount).map((item) => makeRow(item.itemName, item.amount, item.percentage))}
-    </div>
+  const [items, setItems] = useState<classificationType>({ headList: [], itemList: [] })
+
+  useRequest(
+    () => {
+      const date = new Date()
+      return services.statistic.classification({
+        year: date.getFullYear(),
+        month: date.getMonth() + 1,
+        date: date.getDate()
+      })
+    },
+    {
+      onSuccess: (data) => {
+        if (data.success) {
+          setItems(data.data)
+        }
+      }
+    }
   )
+
+  const makeRow = useCallback(
+    (
+      itemName: string,
+      amount: number,
+      percentage: number,
+      bgColor: 'success' | 'warning' | 'danger' = 'warning',
+      hideText: boolean = false
+    ) => {
+      const width = `${ Math.max(items.headList[0].amount, items.headList[1].amount).toString().length * 7.5 }px`
+      const text = hideText ? undefined : `${ percentage }%`
+      return (
+        <div className={ styles.recordStatisticRow } key={ itemName }>
+          <div className={ styles.recordStatisticRowTag }>
+            <Tag shape={ 'round' } size={ 'small' } select>
+              { itemName }
+            </Tag>
+          </div>
+          <div className={ styles.recordStatisticRowProgress }>
+            <Progress
+              size={ 'small' }
+              text={ text }
+              percentage={ percentage }
+              backgroundColor={ bgColor }
+              textColor={ '#000' }
+            ></Progress>
+          </div>
+          <div className={ styles.recordStatisticRowTag }>
+            <Tag size={ 'small' } width={ width }>
+              { amount }
+            </Tag>
+          </div>
+        </div>
+      )
+    },
+    [items.headList]
+  )
+
+  const records = useMemo(
+    () => (
+      <>
+        { items.headList.length &&
+          items.headList.map((item, index) =>
+            makeRow(item.itemName, item.amount, item.percentage, index ? 'success' : 'danger', true)
+          ) }
+        { items.itemList.length &&
+          items.itemList
+            .sort((a, b) => b.amount - a.amount)
+            .map((item) => makeRow(item.itemName, item.amount, item.percentage)) }
+      </>
+    ),
+    [items.headList, items.itemList, makeRow]
+  )
+  return <div className={ styles.recordStatisticContainer }>{ records }</div>
 })
 
 RecordStatistic.displayName = 'RecordStatistic'
--- src/components/biz/record-statistic/RecordStatistic.types.ts
+++ src/components/biz/record-statistic/RecordStatistic.types.ts
@@ -1,3 +1,8 @@
 export interface RecordStatisticProps {}
 
 export interface RecordStatisticRef {}
+
+export type classificationType = {
+  headList: { itemName: string; amount: number; percentage: number }[]
+  itemList: { itemName: string; amount: number; percentage: number }[]
+}
